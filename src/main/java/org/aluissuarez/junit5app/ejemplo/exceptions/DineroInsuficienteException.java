5bf85613c028a99f4561bbf3dbd018a4bd6185d1
lusuareza <lsuarezalfaro@gmail.com>
2023-07-09 12:16:53 +0200

    first commit

---
+++ src/main/java/org/aluissuarez/junit5app/ejemplo/exceptions/DineroInsuficienteException.java
@@ -0,0 +1,9 @@
+package org.aluissuarez.junit5app.ejemplo.exceptions;
+
+public class DineroInsuficienteException extends RuntimeException {
+
+    public DineroInsuficienteException(String message){
+        super(message);
+    }
+
+}
